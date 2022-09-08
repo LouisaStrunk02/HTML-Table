f5cd8b51b13ca257dae29863eb27d31b5695b01a
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-09-08 15:26:37 +0200

    show/hide columns by clicking on the header cell

--- table.js
+++ table.js
@@ -1,4 +1,11 @@
-function getID(columnID) {
-    id = columnID;
-    console.log(id);
+function visibility(columnId) {
+    let contents = document.querySelectorAll(`#` + columnId);
+    for (let i = 1; i < contents.length; i++) {
+        if (contents[i].style.visibility == "hidden") {
+            contents[i].style.visibility = "visible";
+        }
+        else {
+            contents[i].style.visibility = "hidden";
+        }
+    }
 };
