37ce1395f63e67b4a040fb638713a958814dcd63
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-09-14 09:08:04 +0200

    fixed code style and add unique ids

--- table.js
+++ table.js
@@ -1,11 +1,11 @@
 function visibility(columnId) {
-    let contents = document.querySelectorAll(`#` + columnId);
-    for (let i = 1; i < contents.length; i++) {
-        if (contents[i].style.visibility == "hidden") {
-            contents[i].style.visibility = "visible";
-        }
-        else {
-            contents[i].style.visibility = "hidden";
-        }
+  let contents = document.getElementsByName(columnId);
+  for (let i = 0; i < contents.length; i++) {
+    if (contents[i].style.visibility == "hidden") {
+      contents[i].style.visibility = "visible";
     }
+    else {
+      contents[i].style.visibility = "hidden";
+    }
+  }
 };
